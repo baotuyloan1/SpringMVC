3960ae25d873b8162c5b57386de4e823a5ad6c82
unknown <nguyenducbaokey@gmail.com>
2023-04-13 22:40:58 +0700

    ModelAndView and String in Controller

--- src/main/java/edu/fa/controller/LoginController.java
+++ src/main/java/edu/fa/controller/LoginController.java
@@ -1,8 +1,8 @@
 package edu.fa.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import edu.fa.model.Fresher;
 
@@ -10,10 +10,18 @@ import edu.fa.model.Fresher;
 public class LoginController {
 
 //	Value sẽ được mapping với url
+//	Spring 2.0
+//	@RequestMapping(value = "login")
+//	public ModelAndView showLogin() {
+//		ModelAndView mav = new ModelAndView("login");
+//		mav.addObject("fresher", new Fresher("BaoNguyen", "Java"));
+//		return mav;
+//	}
+
+//	SPring 3.0
 	@RequestMapping(value = "login")
-	public ModelAndView showLogin() {
-		ModelAndView mav = new ModelAndView("login");
-		mav.addObject("fresher", new Fresher("BaoNguyen", "Java"));
-		return mav;
+	public String showLogin(Model model) {
+		model.addAttribute("fresher", new Fresher("BaoNguyen", "Java"));
+		return "login";
 	}
 }
